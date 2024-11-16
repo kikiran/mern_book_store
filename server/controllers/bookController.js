602d9cb748db24c3789192863f8b65f6da9d882b
imandi.kumar <imandi.kumar@upbound.com>
2024-11-16 12:17:01 +0530

    added mongodb connection

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -1,8 +1,9 @@
+import mongoose from "mongoose";
 import Book from "../models/bookSchema";
 
 export const getBooks = async (req, res) => {
 	try {
-        const books = await Book.find({})
+		const books = await Book.find({});
 		res.status(200).json({ success: true, data: books });
 	} catch (error) {
 		res.status(404).json({
@@ -10,28 +11,104 @@ export const getBooks = async (req, res) => {
 			message: "Please check something went wrong",
 		});
 	}
+};
 
-export const createBook = async (req, res) => { 
-    const book = req.body;
+export const createBook = async (req, res) => {
+	const book = req.body;
 
-    if(!book.title || !book.author || !book.coverImage || !book.price || !book.published) {
-        res.status(404).json({success: false, message: "All fields are required"});
-    }
-    const addBook = new Book(book);
-    try {
-        await addBook.save();
-        res.status(201).json({success: true, message: "Book saved successfully"});
-    }catch (error) {
-        res.status(500).json({
-            success: false,
-            message: "Please check something went wrong",
-        });
-    }
+	if (
+		!book.title ||
+		!book.author ||
+		!book.coverImage ||
+		!book.price ||
+		!book.published
+	) {
+		res.status(404).json({
+			success: false,
+			message: "All fields are required",
+		});
+	}
+	const addBook = new Book(book);
+	try {
+		await addBook.save();
+		res.status(201).json({
+			success: true,
+			message: "Book saved successfully",
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: "Please check something went wrong",
+		});
+	}
+};
 
-}
+export const updateBook = async (req, res) => {
+	const book = req.body;
+	const { id } = req.params;
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		res.status(404).json({
+			success: false,
+			message: "Book Id is not valid",
+		});
+	}
+	try {
+		const updateBook = await Book.findByIdAndUpdate(id, book, {
+			new: true,
+			runValidators: true,
+		});
 
-export const updateBook = async (req, res) => { 
-    const book = req.body;
-    const { id} = req.params
+		res.status(200).json({
+			success: true,
+			message: "Book updated successfully",
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: "Update book failed",
+		});
+	}
+};
 
-}
+export const deleteBook = async (req, res) => {
+	const { id } = req.params;
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		res.status(404).json({
+			success: false,
+			message: "Book Id is not valid",
+		});
+	}
+
+	try {
+		await Book.findByIdAndDelete(id);
+		res.status(200).json({
+			success: true,
+			message: "Book successfully deleted",
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: "Book Id is not valid",
+		});
+	}
+};
+
+export const getBookById = async (req, res) => {
+	const { id } = req.params;
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		res.status(404).json({
+			success: false,
+			message: "Book Id is not valid",
+		});
+	}
+
+	try {
+		const book = await Book.findOne({});
+		res.status(200).json({ success: true, data: book });
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: "book id is not valid",
+		});
+	}
+};
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 dotenv.config();
 
 const port = process.env.PORT || 8089;
@@ -14,6 +15,16 @@ app.use("/", async (req, res) => {
 	res.status(200).json("Welcome to Book Store app");
 });
 
-app.listen(port, () => {
-	console.log("listening on port " + port);
-});
+const startServer = () => {
+	try {
+		connectDB();
+		app.listen(port, () => {
+			console.log("listening on port " + port);
+		});
+	} catch (error) {
+		console.log("Something wrong with your server", error);
+		process.exit(1);
+	}
+};
+
+startServer();
